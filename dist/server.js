650dbb60b65a87baf322c7e0b69928a0a9ff81e5
Robinson Ody <robinsonody@Robinson-Ody.local>
2020-12-21 15:41:33 +0700

    change folder structure

---
+++ dist/server.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import chalk from 'chalk';
+const app = express();
+const PORT = 8000;
+app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+app.listen(PORT, () => {
+    console.log(chalk.blueBright.bold(`⚡️[server]: Server is running at https://localhost:${PORT}`));
+});
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-// rest of the code remains same
+import chalk from 'chalk';
 const app = express();
 const PORT = 8000;
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+  console.log(chalk.blueBright.bold(`⚡️[server]: Server is running at https://localhost:${PORT}`));
 });
