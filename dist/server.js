1dafc9add9f74042e4d24f1acfb38ea4ccb1f22c
Robinson Ody <robinsonody@Robinson-Ody.local>
2020-12-22 15:04:18 +0700

    nodeModuleResolution

--- dist/server.js
+++ dist/server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import chalk from 'chalk';
 const app = express();
 const PORT = 8000;
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
-app.listen(PORT, () => {
-    console.log(chalk.blueBright.bold(`⚡️[server]: Server is running at https://localhost:${PORT}`));
-});
+app.get('/', (_, res) => res.send('Express + TypeScript Server'));
+app.listen(PORT, () => console.log(chalk.blueBright.bold(`⚡️[server]: Server is running at https://localhost:${PORT}`)));
+//# sourceMappingURL=server.js.map
