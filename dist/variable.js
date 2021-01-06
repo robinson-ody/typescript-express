bcf9a66c735cb453e4ebc67bcf4543035cfa6eaa
Robinson Ody <robinsonody@Robinson-Ody.local>
2021-01-06 19:11:45 +0700

    Remove dist/

--- dist/variable.js
+++
@@ -1,2 +0,0 @@
-export const secretKey = 'FreeSpin2020-Robin-Andri-Leo-Hendra-FirstStack';
-export const mainUrl = process.env.NODE_ENV === 'production' ? 'http://localhost:8000' : 'http://localhost:8000';
